ecf9a54c8e8f55f3f39914e59164ffc766414749
Ethorcharl <zenja_riga5@yahoo.com>
2023-06-29 11:53:10 +0300

    Added functionality for modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from './model/employee';
 import { EmployeeService } from './service/employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -12,6 +13,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class AppComponent implements OnInit{
   //title = 'AngularEmployeeApp.UI';
   public employees?: Employee[];
+  public editEmployee?:Employee | null;
+  public deleteEmployee?:Employee|null;
 
   constructor(private employeeService:EmployeeService) { }
 
@@ -19,6 +22,7 @@ export class AppComponent implements OnInit{
     this.getEmployees();
   }
 
+  //get employee
   public getEmployees():void { //without next: and error: subscribe dont work
     this.employeeService.getEmployee().subscribe(
       {
@@ -30,6 +34,71 @@ export class AppComponent implements OnInit{
       }
     }
     );
+  }
+  //add employee
+  public onAddEmployee(addForm:NgForm): void {
+    document.getElementById('add-employee-form')?.click(); // close modal
+    this.employeeService.addEmployee(addForm.value).subscribe( //subscribe to be notified if something is returned from the server
+     { next: (response: Employee)=>{
+        console.log(response);
+        this.getEmployees();
+        addForm.reset(); // delete info in fields
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+        addForm.reset(); // delete info in fields
+      }
+    }
+    );
+  }
+
+  //update employee
+  public onUpdateEmployee(employee:Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe( //subscribe to be notified if something is returned from the server
+     { next: (response: Employee)=>{
+        console.log(response);
+        this.getEmployees(); //reload page
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    }
+    );
+  }
+
+    //delete employee
+  public onDeleteEmployee(employeeId:number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe( //subscribe to be notified if something is returned from the server
+     { next: (response: void)=>{
+        console.log(response);
+        this.getEmployees(); //reload page
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    }
+    );
+  }
+
+  //search employy
+  public searchEmployees(key:string):void {
+    console.log(key);
+    const results:Employee[]=[]; //initialize empty array
+    for (const employee of this.employees!) //loop
+    {
+      //indexOf main 
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase())!==-1 // -1 if it is not present
+      || employee.email.toLowerCase().indexOf(key.toLowerCase())!==-1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase())!==-1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase())!==-1)
+      {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if(results.length===0 || !key){
+      this.getEmployees(); // need change it
+    }
   }
 
   //for Modal
@@ -43,9 +112,11 @@ export class AppComponent implements OnInit{
       button.setAttribute('data-target','#addEmployeeModal');
     }
     if(mode ==='edit'){
+      this.editEmployee=employee;
       button.setAttribute('data-target','#updateEmployeeModal');
     }
     if(mode ==='delete'){
+      this.deleteEmployee=employee;
       button.setAttribute('data-target','#deleteEmployeeModal');
     }
     container?.appendChild(button);//? now it's button exist in html?
